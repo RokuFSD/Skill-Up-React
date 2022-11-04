1495ffdcb0b07387f613268ed6908679f387aa2f
RokuFSD <emilianordx@gmail.com>
2022-11-04 16:53:33 -0300

    Edited middleware to execute on any movement

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,8 +1,12 @@
 import { createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
 import { getAccount } from '../features/user/accountActions.js';
+import { deposit, withdraw, transaction } from '../features/user/balanceActions.js';
 
 export const listenerMiddleware = createListenerMiddleware();
 listenerMiddleware.startListening({
-  matcher: isAnyOf(getAccount.fulfilled),
-  effect: (action, listenerApi) => localStorage.setItem('user', JSON.stringify(listenerApi.getState().user.user))
-})
+  matcher: isAnyOf(getAccount.fulfilled, deposit.fulfilled, withdraw.fulfilled, transaction.fulfilled),
+  effect: (action, listenerApi) => {
+    localStorage.setItem('user', JSON.stringify(listenerApi.getState().user.user));
+    localStorage.setItem('userToken', listenerApi.getState().user.userToken);
+  }
+});
