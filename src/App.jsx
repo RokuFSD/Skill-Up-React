2c322e7faa5a9398e54e30bc966873f9f88b374b
RokuFSD <emilianordx@gmail.com>
2022-11-04 16:17:05 -0300

    Added main element and Outlet on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
