a7a451b84cede6c4d15fca6a18755d463397e012
RokuFSD <emilianordx@gmail.com>
2022-11-05 18:20:50 -0300

    Added styles to the dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,21 @@
 import BalanceCard from '../balance/BalanceCard.jsx';
+import BalanceActions from '../balance/BalanceActions.jsx';
+import DashBoardTitle from './DashBoardTitle.jsx';
 
 function Dashboard() {
   return (
-    <div className=''>
-      <BalanceCard />
-    </div>
+    <section className='flex flex-col gap-12 h-full px-52'>
+      <DashBoardTitle />
+      <div className='flex w-full'>
+        <div className='bg-neutral-50 flex flex-col gap-12 shadow-2xl p-4 w-96 h-72 rounded-lg'>
+          <BalanceCard />
+          <BalanceActions />
+        </div>
+        <div className='w-96'>
+          {/*Resume Movements */}
+        </div>
+      </div>
+    </section>
   );
 }
 
