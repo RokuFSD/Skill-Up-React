2010e689cd8a0501ae0ea8ec5161f62e5cb9a262
RokuFSD <emilianordx@gmail.com>
2022-11-05 18:19:03 -0300

    Added new svg

---
+++ src/components/svg/ShoppingBag.jsx
@@ -0,0 +1,10 @@
+function ShoppingBagSvg() {
+  return (
+    <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+      <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2'
+            d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z'></path>
+    </svg>
+  );
+}
+
+export default ShoppingBagSvg;
