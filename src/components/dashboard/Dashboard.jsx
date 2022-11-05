5214fb623fae7d61539352257c59b86544efa0c5
RokuFSD <emilianordx@gmail.com>
2022-11-05 16:23:03 -0300

    Created the balance card

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import BalanceCard from '../balance/BalanceCard.jsx';
+
+function Dashboard() {
+  return (
+    <div className=''>
+      <BalanceCard />
+    </div>
+  );
+}
+
+export default Dashboard;
