9d4f6383ee194db8f23809327e1814ac144614d1
RokuFSD <emilianordx@gmail.com>
2022-11-09 01:51:13 -0300

    Implemented transactions feature

--- src/pages/MovementsPage.jsx
+++ src/pages/MovementsPage.jsx
@@ -1,21 +1,39 @@
-import { useGetTransactionsQuery } from '../features/movement/transactionSlice.js';
-import { infinite } from '../features/movement/transactionSlice.js';
-import { useState } from 'react';
+import {
+  infinite,
+  useGetAllTransactionsQuery,
+  useGetTransactionsQuery
+} from '../features/movement/transactionSlice.js';
+import { useEffect, useState } from 'react';
+import { filterData } from '../utils/functionUtils.js';
 import InfiniteScroll from '../components/list/InfiniteScroll.jsx';
+import Searchbar from '../components/list/Searchbar.jsx';
 
 function MovementsPage() {
   const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [triggeredSearch, setTriggeredSearch] = useState(false);
+
   const { data, isFetching } = useGetTransactionsQuery(page);
   const { isFetching: isNextDataFetching } = useGetTransactionsQuery(page + 1, { skip: !data?.nextPage });
-  const result = infinite(page);
+  const { data: allData, isFetching: isAllFetching } = useGetAllTransactionsQuery('', { skip: !triggeredSearch });
+
+  let result = infinite(page);
+
+  useEffect(() => {
+    if (isAllFetching || !triggeredSearch) return;
+    if (query === '') setTriggeredSearch(false);
+    setFilteredData(filterData(allData, query));
+  }, [triggeredSearch, query, isAllFetching]);
 
   return (
-    <div className='h-full w-full flex items-center justify-center'>
-      <InfiniteScroll fetching={isFetching || isNextDataFetching}
+    <div className='h-full w-full flex flex-col items-center justify-center gap-10'>
+      <Searchbar setQuery={setQuery} setTriggeredSearch={setTriggeredSearch} />
+      <InfiniteScroll fetching={triggeredSearch ? isAllFetching : (isFetching || isNextDataFetching)}
                       page={page}
-                      items={result}
-                      loadMore={() => setPage((prevState) => prevState + 1)}
-                      hasMore={data?.nextPage}
+                      items={triggeredSearch ? filteredData : result}
+                      loadMore={triggeredSearch ? null : () => setPage((prevState) => prevState + 1)}
+                      hasMore={triggeredSearch ? false : data?.nextPage}
       />
     </div>
   );
