8518a5ab1a48252b4e1273b16cac76579b3fe23d
RokuFSD <emilianordx@gmail.com>
2022-11-09 01:49:25 -0300

    New infinite scroll logic

--- src/components/list/InfiniteScroll.jsx
+++ src/components/list/InfiniteScroll.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef } from 'react';
+import EmptyList from './EmptyList.jsx';
 import TransactionCard from './TransactionCard.jsx';
+import TransactionList from './TransactionList.jsx';
+import SkeletonTransaction from '../skeleton/SkeletonTransaction.jsx';
 
 function getScrollBottom(element) {
   let scrollTop = element.scrollTop;
@@ -12,7 +15,7 @@ function InfiniteScroll({ page, items, loadMore, hasMore, fetching }) {
   const ref = useRef();
 
   useEffect(() => {
-    if (!ref?.current) return;
+    if (!ref?.current || !hasMore) return;
     let isSend = true;
 
     function handleScroll() {
@@ -26,20 +29,18 @@ function InfiniteScroll({ page, items, loadMore, hasMore, fetching }) {
     return () => ref.current?.removeEventListener('scroll', handleScroll);
   }, [items, page, hasMore, ref.current]);
 
-  if (fetching && (items.length < 2)) return <div>Loading...</div>;
+  if (fetching && (items?.length < 2)) {
+    return (
+      <TransactionList ref={null}>
+        {[...Array(5)].map((_, index) => (<SkeletonTransaction key={index} />))}
+      </TransactionList>
+    );
+  }
 
   return (
-    <section
-      ref={ref}
-      className='h-128 w-128 overflow-y-scroll overflow-x-hidden shadow-xl
-      border scroll-smooth will-change-scroll
-      scrollbar-thin
-      scrollbar-thumb-neutral-300 scrollbar-track-transparent'>
-      {items.map((item) => (
-        <TransactionCard key={item.id} {...item} />
-      ))}
-      {(fetching && items?.length) && <div>Loading...</div>}
-    </section>
+    <TransactionList ref={ref}>
+      {items?.length === 0 ? <EmptyList /> : items?.map((item) => <TransactionCard key={item?.id} {...item} />)}
+    </TransactionList>
   );
 }
 
