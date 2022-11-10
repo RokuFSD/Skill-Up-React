e8f8c17dfb2b47bf2b11663e6d9f5bdba4943fc1
RokuFSD <emilianordx@gmail.com>
2022-11-10 13:47:03 -0300

    Added filter functionality

--- src/pages/MovementsPage.jsx
+++ src/pages/MovementsPage.jsx
@@ -3,37 +3,56 @@ import {
   useGetAllTransactionsQuery,
   useGetTransactionsQuery
 } from '../features/movement/transactionSlice.js';
+import { filtersArr } from '../components/filter/FilterBar.jsx';
 import { useEffect, useState } from 'react';
-import { filterData } from '../utils/functionUtils.js';
-import InfiniteScroll from '../components/list/InfiniteScroll.jsx';
+import { filterByType, filterData, orderByDate } from '../utils/functionUtils.js';
 import Searchbar from '../components/list/Searchbar.jsx';
+import FilterBar from '../components/filter/FilterBar.jsx';
+import InfiniteScroll from '../components/list/InfiniteScroll.jsx';
 
 function MovementsPage() {
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
+  const [filters, setFilters] = useState(filtersArr);
   const [filteredData, setFilteredData] = useState([]);
   const [triggeredSearch, setTriggeredSearch] = useState(false);
+  const [triggeredFilter, setTriggeredFilter] = useState(false);
 
   const { data, isFetching } = useGetTransactionsQuery(page);
   const { isFetching: isNextDataFetching } = useGetTransactionsQuery(page + 1, { skip: !data?.nextPage });
-  const { data: allData, isFetching: isAllFetching } = useGetAllTransactionsQuery('', { skip: !triggeredSearch });
+  const {
+    data: allData,
+    isFetching: isAllFetching
+  } = useGetAllTransactionsQuery('', { skip: !triggeredSearch || !data?.nextPage });
 
   let result = infinite(page);
 
   useEffect(() => {
+    let filtered = allData;
     if (isAllFetching || !triggeredSearch) return;
     if (query === '') setTriggeredSearch(false);
-    setFilteredData(filterData(allData, query));
-  }, [triggeredSearch, query, isAllFetching]);
+    if (triggeredFilter && triggeredSearch) setTriggeredSearch(true);
+    filtered = orderByDate(filtered?.slice(), filters[0].value);
+    filtered = filterByType(filtered, filters[1].value);
+    setFilteredData(filterData(filtered, query));
+  }, [triggeredSearch, query, isAllFetching, filters]);
 
   return (
-    <div className='h-full w-full flex flex-col items-center justify-center gap-10'>
-      <Searchbar setQuery={setQuery} setTriggeredSearch={setTriggeredSearch} />
-      <InfiniteScroll fetching={triggeredSearch ? isAllFetching : (isFetching || isNextDataFetching)}
-                      page={page}
-                      items={triggeredSearch ? filteredData : result}
-                      loadMore={triggeredSearch ? null : () => setPage((prevState) => prevState + 1)}
-                      hasMore={triggeredSearch ? false : data?.nextPage}
+    <div className='h-full w-128 flex flex-col items-center justify-center mx-auto gap-8'>
+      <div className="w-full flex flex-col gap-3 border-b py-2">
+        <Searchbar setQuery={setQuery} setTriggeredSearch={setTriggeredSearch} />
+        <FilterBar filters={filters}
+                   setFilters={setFilters}
+                   setTriggeredSearch={setTriggeredSearch}
+                   setTriggeredFilter={setTriggeredFilter}
+        />
+      </div>
+      <InfiniteScroll
+        page={page}
+        items={triggeredSearch ? filteredData : result}
+        hasMore={triggeredSearch ? false : data?.nextPage}
+        fetching={triggeredSearch ? isAllFetching : (isFetching || isNextDataFetching)}
+        loadMore={triggeredSearch ? null : () => setPage(page + 1)}
       />
     </div>
   );
