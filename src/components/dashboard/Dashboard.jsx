2c4967ce6b28143b45fe55a683d7e6d2fdcbfbcc
RokuFSD <emilianordx@gmail.com>
2022-11-10 22:04:10 -0300

    Added send again section

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,40 +1,51 @@
 import BalanceCard from '../balance/BalanceCard.jsx';
 import BalanceActions from '../balance/BalanceActions.jsx';
 import DashBoardTitle from './DashBoardTitle.jsx';
-import TransactionList from '../list/transaction/TransactionList.jsx';
+import ItemScroll from '../list/ItemScroll.jsx';
 import TransactionCard from '../list/transaction/TransactionCard.jsx';
 import SkeletonTransaction from '../skeleton/SkeletonTransaction.jsx';
 import Arrow from '../svg/Arrow.jsx';
-import { useGetTransactionsQuery } from '../../features/movement/transactionSlice.js';
+import {
+  useGetLastThreeAccountsQuery,
+  useGetTransactionsQuery
+} from '../../features/transaction/transactionSlice.js';
 import { Link } from 'react-router-dom';
-import { useGetAllAccountsQuery } from '../../features/movement/accountSlice.js';
+import { useGetAllAccountsQuery } from '../../features/transaction/accountSlice.js';
+import DashboardUsers from './DashboardUsers.jsx';
 
 function Dashboard() {
   useGetAllAccountsQuery('');
   const { data, isFetching } = useGetTransactionsQuery(1);
+  const { data: lastAccounts, isFetching: lastFetching } = useGetLastThreeAccountsQuery();
 
   return (
     <section className='flex flex-col gap-12 h-full px-52'>
       <DashBoardTitle />
       <div className='flex w-full justify-center gap-20'>
-        <div className='bg-neutral-50 flex flex-col gap-12 shadow-2xl p-4 w-96 h-72 rounded-lg'>
-          <BalanceCard />
-          <BalanceActions />
+        <div className='flex flex-col h-full justify-between'>
+          <div className='bg-neutral-50 flex flex-col gap-12 shadow-lg p-4 w-96 h-72 rounded-lg'>
+            <BalanceCard />
+            <BalanceActions />
+          </div>
+          <div className='bg-neutral-50 flex shadow-lg p-4 w-96 h-40 rounded-lg flex-wrap'>
+            <h2 className='text-xl font-semibold text-neutral-500 basis-full'>Send again</h2>
+            <DashboardUsers isFetching={lastFetching} accounts={lastAccounts} />
+          </div>
         </div>
         <div className='bg-neutral-50 p-4 rounded-xl shadow-xl'>
           <h2 className='text-xl font-semibold text-neutral-500'>Last activity</h2>
           {isFetching ? (
-            <TransactionList large>
+            <ItemScroll large>
               {[...Array(5)].map((_, index) => (
                 <SkeletonTransaction key={index} />
               ))}
-            </TransactionList>
+            </ItemScroll>
           ) : (
-            <TransactionList large>
+            <ItemScroll large>
               {data.data?.slice(0, 5).map((transaction) => (
                 <TransactionCard key={transaction.id} {...transaction} />
               ))}
-            </TransactionList>
+            </ItemScroll>
           )}
           <Link to={'/movements'} className='text-indigo-600 font-semibold hover:text-indigo-400 transition-all'>
             <div className='w-full flex items-center justify-between'>
