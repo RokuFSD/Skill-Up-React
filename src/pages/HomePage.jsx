4778fab91de0594670737a342e6675b5762dd913
RokuFSD <emilianordx@gmail.com>
2022-11-11 20:47:59 -0300

    New 404 page working

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import Dashboard from '../components/dashboard/Dashboard.jsx';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../features/user/userSlice.js';
 import { Navigate } from 'react-router-dom';
@@ -6,7 +5,7 @@ import { Navigate } from 'react-router-dom';
 function HomePage() {
   const user = useSelector(selectUser);
   /* Instead of navigate to login page, we can show a home page with information about the app */
-  return !user ? <Navigate to={'/login'} /> : <Dashboard />;
+  return !user ? <Navigate to={'/login'} /> : <Navigate to={'/balance'} />;
 }
 
 export default HomePage
