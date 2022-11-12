9bc62cb1225f115baa62534adff43d7ebd42fcd5
RokuFSD <emilianordx@gmail.com>
2022-11-12 02:03:08 -0300

    Fix logout persist data

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -4,22 +4,24 @@ import { authRegister } from '../features/user/authActions.js';
 import { deposit, withdraw, transaction } from '../features/user/balanceActions.js';
 import { apiSlice } from '../features/api/apiSlice.js';
 import store from './store.js';
+import { userLogout } from '../features/user/userSlice.js';
 
 export const listenerMiddleware = createListenerMiddleware();
 export const adminListenerMiddleware = createListenerMiddleware();
+export const logoutListenerMiddleware = createListenerMiddleware();
+export const transactionListenerMiddleware = createListenerMiddleware();
 listenerMiddleware.startListening({
   matcher: isAnyOf(
     deposit.fulfilled,
     withdraw.fulfilled,
     transaction.fulfilled,
     authRegister.fulfilled,
-    getAccount.fulfilled,
+    getAccount.fulfilled
   ),
   effect: (action, listenerApi) => {
     localStorage.setItem('user', JSON.stringify(listenerApi.getState().user.user));
     localStorage.setItem('account', JSON.stringify(listenerApi.getState().user.account));
     localStorage.setItem('userToken', listenerApi.getState().user.userToken);
-    store.dispatch(apiSlice.util.invalidateTags(['Transaction']))
   }
 });
 
@@ -27,5 +29,19 @@ adminListenerMiddleware.startListening({
   matcher: isAnyOf(adminResponse.fulfilled),
   effect: (action, listenerApi) => {
     localStorage.setItem('adminToken', listenerApi.getState().user.adminToken);
+  }
+});
+
+transactionListenerMiddleware.startListening({
+  matcher: isAnyOf(transaction.fulfilled, withdraw.fulfilled, deposit.fulfilled),
+  effect: (action, listenerApi) => {
+    store.dispatch(apiSlice.util.invalidateTags(['Transaction']));
+  }
+});
+
+logoutListenerMiddleware.startListening({
+  matcher: isAnyOf(userLogout),
+  effect: (action, listenerApi) => {
+    store.dispatch(apiSlice.util.resetApiState());
   }
 });
