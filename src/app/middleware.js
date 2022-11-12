2a37f2d2146f55c72d47cafa0def735de80272c7
RokuFSD <emilianordx@gmail.com>
2022-11-12 01:16:20 -0300

    Fixed cache problem

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -2,6 +2,8 @@ import { createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
 import { adminResponse, getAccount } from '../features/user/accountActions.js';
 import { authRegister } from '../features/user/authActions.js';
 import { deposit, withdraw, transaction } from '../features/user/balanceActions.js';
+import { apiSlice } from '../features/api/apiSlice.js';
+import store from './store.js';
 
 export const listenerMiddleware = createListenerMiddleware();
 export const adminListenerMiddleware = createListenerMiddleware();
@@ -17,6 +19,7 @@ listenerMiddleware.startListening({
     localStorage.setItem('user', JSON.stringify(listenerApi.getState().user.user));
     localStorage.setItem('account', JSON.stringify(listenerApi.getState().user.account));
     localStorage.setItem('userToken', listenerApi.getState().user.userToken);
+    store.dispatch(apiSlice.util.invalidateTags(['Transaction']))
   }
 });
 
