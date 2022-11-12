6c51339bdde5feae1688d27137b2f21f2611254f
RokuFSD <emilianordx@gmail.com>
2022-11-12 01:16:43 -0300

    Implemented user selection on send form

--- src/components/send/send.jsx
+++ src/components/send/send.jsx
@@ -1,9 +1,12 @@
 import SendPage from '../../pages/SendPage';
 import MoneyForm from '../moneyForm/moneyForm';
+import { useSelector } from 'react-redux';
+import { selectDestinyAccount } from '../../features/transaction/transactionSlice.js';
 
 const Send = () => {
+  const destinyAccount = useSelector(selectDestinyAccount);
   return (
-    <MoneyForm screen="send">
+    <MoneyForm screen="send" destinyAccount={destinyAccount}>
       <SendPage />
     </MoneyForm>
   );
